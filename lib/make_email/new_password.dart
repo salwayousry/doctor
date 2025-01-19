67ad22a9e82380817ce72e26c99bbc408ce04210
Salma Elbasha <salma.abobasha1@gmail.com>
2025-01-19 23:17:51 +0200

    updates

--- lib/make_email/new_password.dart
+++ lib/make_email/new_password.dart
@@ -86,7 +86,7 @@ class NewPasswordPage extends StatelessWidget {
                         }
                       },
                       style: ElevatedButton.styleFrom(
-                        primary: const Color(0xFF19649E),
+                        backgroundColor: const Color(0xFF19649E),
                         minimumSize: const Size(double.infinity, 50),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8),
