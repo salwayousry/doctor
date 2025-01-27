bf8df4d70e726572208ace7a78a844351ff241d8
Salma Elbasha <m.elgamal0551@gmail.com>
2025-01-27 14:31:01 +0200

    updates

--- lib/core/strings.dart
+++
@@ -1,3 +0,0 @@
-const String SpSignUpSuccessMsg = "تم انشاء الحساب بنجاح , يرجى تسجيل الدخول";
-const String SpSignUpErrorMsg = 'خطأ في عملية التسجيل ';
-const String SpSignUpLoadingMsg = "جاري تسجيل طلبك , انتظر قليلا";
